3c79bef1056dadce6bfe871c0a5f2e2b1c60d326
m3gaf3di <amineblood3@gmail.com>
2024-03-15 16:10:19 +0100

    updated the POST and the PUT routes

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); // This middleware is used to parse the request body
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
 
 const todos = [{id: 1, title: 'First todo', description: 'This is the first todo'}]; // This will act as a simple in-memory database
 
@@ -14,23 +15,27 @@ app.get('/todos', (req, res) => {
     res.json(todos);
 });
 
-// Add more routes here for creating, updating, and deleting todos
 // Add a new todo
+let nextId = todos.length + 1;
+
 app.post('/todos', (req, res) => {
     const { title, description } = req.body;
-    const newTodo = { title, description };
+    const newTodo = { id: nextId++, title, description }; // Assign an ID
     todos.push(newTodo);
-    res.json(newTodo);
+    res.status(201).json(newTodo); // Send 201 Created status code
 });
+
+// Update a todo
 app.put('/todos/:id', (req, res) => {
-    const id = req.params.id;
+    const id = parseInt(req.params.id, 10); // Convert to number
     const { title, description } = req.body;
     const todo = todos.find(todo => todo.id === id);
+
     if (!todo) {
         return res.status(404).send('The todo with the given ID was not found.');
     }
+
     todo.title = title;
     todo.description = description;
     res.json(todo);
 });
-
